6a7442c38d8f9a6b16f9287e64b73721a0ab632a
Rustamovs1 <rustamovaliyor3@gmail.com>
2024-01-15 16:32:10 +0500

    kjioj

---
+++ Githup/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Githup project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Githup.settings')
+
+application = get_wsgi_application()
